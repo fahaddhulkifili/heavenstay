75a4fc6519187d2fcdcfb6eac6dc04a83d159f6c
Fahad <alfahd360@gmail.com>
2023-11-13 16:20:53 +0000

    Initial commit from gatsby

---
+++ gatsby-config.js
@@ -0,0 +1,10 @@
+/**
+ * @type {import('gatsby').GatsbyConfig}
+ */
+module.exports = {
+  siteMetadata: {
+    title: `heavenstay`,
+    siteUrl: `https://www.yourdomain.tld`
+  },
+  plugins: ["gatsby-plugin-sass"]
+};
